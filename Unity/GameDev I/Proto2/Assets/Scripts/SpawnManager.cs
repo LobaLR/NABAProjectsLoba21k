698fc8f77d9fb38e587efa90805fa7f68c71090d
Loba L. Ricotti <loba.inferyum@gmail.com>
2022-04-21 15:27:11 +0200

    Proto2 Update

--- Unity/GameDev I/Proto2/Assets/Scripts/SpawnManager.cs
+++ Unity/GameDev I/Proto2/Assets/Scripts/SpawnManager.cs
@@ -13,11 +13,13 @@ public class SpawnManager : MonoBehaviour
 
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     private void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -26,10 +28,16 @@ public class SpawnManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S)) 
         {
             // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
     }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }                                                                                                                                                                                         
 
 }
